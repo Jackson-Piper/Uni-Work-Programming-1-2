cd714d5086a4ab5fb1333ffe5246477e6f27ff92
Jackson-Piper <80015676+Jackson-Piper@users.noreply.github.com>
2021-12-21 15:08:47 +1100

    Create task2.java
    task 2 ass 2 porj 1

---
+++ task2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class task2{
+
+
+  Public static void main(String[] args){
+    Scanner sc = new SCanner(System.in)
+    
+      System.out.println("How many days were you recording for?");
+      int length = sc.nextInt();
+        if (length !>0){
+        System.out.println("Error input invalid. Please enter a positive number (i.e 3, 5, 8)");
+        length = sc.nextInt();
+        sc.nextLine();
+        }
+      int cases[] = new int[length];
+      
+      for (int x =0; x<length.length; x++){
+        System.out.println("Please enter the number of cases for day "+(x+1)+": ");
+         cases[x]=sc.nextInt();
+         sc.nextLine();
+          if (!<
+      }
+
+
+
+
+  sc.close;
+  }
+
+}
