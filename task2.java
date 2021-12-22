0f6f0f1881c32bdfeb2787d7d6559e4cd68e97fd
Jackson-Piper <80015676+Jackson-Piper@users.noreply.github.com>
2021-12-22 15:50:45 +1100

    Update task2.java
    Working while chatting, notes for task 2/ code to use

--- task2.java
+++ task2.java
@@ -17,12 +17,44 @@ public class task2{
       
       for (int x =0; x<length.length; x++){
         System.out.println("Please enter the number of cases for day "+(x+1)+": ");
+        
          cases[x]=sc.nextInt();
          sc.nextLine();
-          if (!<
+
+          if (cases[x]!<0){
+          System.out.println("Error please enter a positive number:")
+            cases[x] = sc.nextInt90;
+            sc.nextLine();
+          }
+        
       }
-
-
+    for (int i = 0; i<length.length; i++;){
+    System.out.println(Cases[i]);
+    }
+    int max;
+    int min;
+    for (int i = 0; i<length.length; i++;){
+     if (max< cases[i]){
+      max = cases;
+     }
+     if (min>cases[i]){
+     min = cases[i];
+     }
+    }
+    
+    
+    
+    
+   for (int i = 0; i<length.length; i++){ 
+    int avgTemp += cases[i] 
+    counter++;
+  }
+    int avg = avtemp/counter;
+    
+    
+    
+   System.out.println("Max case numbers: "+max);
+   System.out.println("Min case numbers: "+min);
 
 
   sc.close;
