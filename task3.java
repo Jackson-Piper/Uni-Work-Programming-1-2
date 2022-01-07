0c44e5f95a08a6291e8e42853dc3a3a5578e7e85
Jackson-Piper <80015676+Jackson-Piper@users.noreply.github.com>
2022-01-07 12:58:47 +1100

    Create task3.java

---
+++ task3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+System.out.println("Please select an option"+
+                   "\n---------------------------------------"+
+                   "\n1. Set canvas size." +
+                   "\n2. Draw shape w/o border." +
+                   "\n3. Draw shape with border." +
+                   "\n4. Clear Canvas." +
+                   "\n5. Exit program."+
+                   "\n---------------------------------------"+
+if (sc.hasNextInt()&&sc.nextInt()<6&&sc.nextInt()&&){
+  int menuChoice = sc.nextInt;
+}
+while(){
+  System.out.print("Error please enter a number between 1-5
+}
+                   
+
+                   
