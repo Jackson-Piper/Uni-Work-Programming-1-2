fe2ceab9007ed1f556a5fa05a3290688fe93dc8d
Jackson-Piper <80015676+Jackson-Piper@users.noreply.github.com>
2023-04-12 16:21:49 +1000

    assignment work 12/4

--- FPAssignment2/login.java
+++ FPAssignment2/login.java
@@ -1,5 +1,9 @@
 public class login{
+
+    ArrayList<User> Users = new ArrayList<>();
+    User currentUser;
     public void main(String[] Args){
+    loadUsers()
     mainMenu()
     }
         public void mainMenu(){
@@ -13,12 +17,25 @@ public class login{
             case "1"{
                 System.out.println("Please enter your username: ")
                 String username = sc.nextline();
-                User user = findUser(username);
+                while(!validateUser(username)){
+                    System.out.println("Error user doesnt exists");
+                    System.out.println("Please check username or enter 'Menu' to return to main menu");
+                    username = sc.nextLine();
+                    if(username.equalsIgnoreCase("Menu")){
+                        mainMenu();
+                    }                
+                }
+
+                User userTemp = findUser(username);
                 System.out.println("Please enter your password: ")
                 String password = sc.nextLine();
                 if(password.equals(user.getPassword())){
-                    login(user);
+                    currentUser = userTemp;
+                    login();
+                }else{
+                    System.out.println("Error wrong password or email")
                 }
+
             }   
             case "2"{
                 System.out.println("Please enter a username for the account: ");
@@ -34,18 +51,20 @@ public class login{
                 int userID = getNextID();
                 User newUser = new Users(userID,username, password, firstName, lastName);
                 user.add(newUser);
+                currentUser = newUser;
                 login(newUser)
             }
             case "3"{
+                saveData();
                 System.exit(0);
             }
         }
 
     }
 
-    public void loginMenu(User user){
+    public void loginMenu(){
         System.out.println("--------------------");
-        System.out.println(user.getName()+" "+user.getSurname());
+        System.out.println(currentUser.getName()+" "+currentUser.getSurname());
         System.out.println("--------------------");
         System.out.println("1)\tUser Details");
         System.out.println("2)\tView records");
@@ -56,23 +75,24 @@ public class login{
          System.out.println("--------------------");
     }
 
-    public void login(User user){
-        loginMenu(user);
+    public void login(){
+        loginMenu();
         String nav = sc.nextLine();
         switch(nav){
             case "1"{
-                userDetails(user);
+                userDetails();
             }
             case "2"{
-                viewRecords(user);
+                viewRecords();
             }
             case "3"{
-                createRecord(user);
+                createRecord();
             }
             case "4"{
-                editRecord(user);
+                editRecord();
             }
             case "5"{
+                saveData();
                 System.exit(0);
             }
             case "6"{
@@ -81,10 +101,61 @@ public class login{
         }
     }
 
-    public void userDetails(User user){
-         System.out.println("User Name : \t" + user.getUserName());
-         System.out.printlm("First Name: \t"+ user.getFirstName());
-         System.out.println("Surname: \t"+ user.getSurname());
+    public void createRecord(){
+        Double weight;
+        Double tempreture;
+        Double bloodPresure;
+        String note;
+        System.out.println("Enter Weight (Kg) [Y/N]: ");
+        String nav = sc.nextLine();
+        if(yesVal(nav)){
+        System.out.println("Weight: ");
+        weight = sc.nextLine();
+        }
+        System.out.println("Enter Tempreture (Celcius) [Y/N]: ");
+        if(yesVal(nav)){
+        System.out.println("Tempreture: ");
+        tempreture = sc.nextLine();
+        }
+        System.out.println("Enter Blood pressure (mmHg) [Y/N]: ");
+        if(yesVal(nav)){
+        System.out.println("Blood Pressure: ");
+        bloodPresure = sc.nextLine();
+        }
+        System.out.println("Would you like to add a note (50 Words) [Y/N]:");
+        if(yesVal(nav)){
+        System.out.println("Note: ");
+        note = sc.nextLine();
+        }
+        DateTime date = getDateTime();
+        String recordID = getNextRecodID();
+        System.out.println("--------------------------");
+        System.out.println("Record:\t"+recordID);
+        System.out.println("Weight:\t"+weight);
+        System.out.println("Tempreture:\t"+tempreture);
+        System.out.println("Blood Presure:\t"+bloodPresure);
+        System.out.println("Note:\t"+note);
+        System.out.println("--------------------------");
+        System.out.println("Would you like to add this record [Y/N]");
+        String commit = sc.nextLine();
+        if(isYes(commit)){
+        Record newRecord = new Record(weight, tempreture, bloodPresure, note);
+        currentUser.addNewRecord(newRecord);
+        System.out.println("Reccord Sucessfully added");
+        System.out.println("Reeturning to menu");
+        login();
+        }else{
+            System.out.println("Record Deleted");
+            System.out.println("Returning to menu");
+        }
+
+
+    }
+
+    public void userDetails(){
+         System.out.println("User Name : \t" + currentUser.getUserName());
+         System.out.printlm("First Name: \t"+ currentUser.getFirstName());
+         System.out.println("Surname: \t"+ currentUser.getSurname());
          System.out.println("Edit? [Y/N]");
          String nav = sc.nextline();
          switch(nav){
@@ -97,46 +168,62 @@ public class login{
                 nav = sc.nextLine();
                 switch (nav){
                     case "1"{
-                        editName(user);
-                        userDetails(user);
+                        editName();
+                        userDetails();
                     }
                     case "2"{
-                        editSurname(user);
-                        userDeatils(user);
+                        editSurname();
+                        userDeatils();
                     }
                     case "3"{
-                        userDeatils(user);
+                        userDeatils();
                     }
                 }
                }
                case "N""n"{
-                login(user);
+                login();
                }
             }
          }
     }
 
-    public void editName(User user){
-        System.out.println("Change "+user.getName()+" to");
+    public void editName(){
+        System.out.println("Change "+currentUser.getName()+" to");
         String name = sc.nextLine();
         user.setName(name);
     }
 
-    public void editSurname(User user){
-        System.out.println("Change "+user.getSurname()+" to");
+    public void editSurname(){
+        System.out.println("Change "+currentUser.getSurname()+" to");
         String name = sc.nextLine();
-        user.setSurname(name);
+        currentUser.setSurname(name);
     }
 
     public void viewRecords(User user){
-        System.out.println(user.getPrintRecords());
+        System.out.println(currentUser.getPrintRecords());
     }
 
     public User findUser(String username){
         for (User user : Users){
-            if(username.equals(user.getUsername))
-            r
+            if(username.equals(user.getUsername)){
+                return user;
+            }
         }
+    }
+
+    public boolean validateUser(String username){
+        for (User user : Users){
+            if(username.equals(user.getUsername)){
+                return true;
+            }else{
+                continue;
+            }
+        }
+        return false;
+    }
+
+    public String getNextID(){
+        return Users.size() + 1
     }
 
 }
