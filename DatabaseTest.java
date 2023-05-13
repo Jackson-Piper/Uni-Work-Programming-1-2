a3f691073fa2fc780377ed650eab7faa27a89b89
Jackson-Piper <80015676+Jackson-Piper@users.noreply.github.com>
2023-05-13 16:29:48 +1000

    sql stuff

--- DatabaseTest.java
+++ DatabaseTest.java
@@ -43,46 +43,89 @@ public void createRecordTable(){
             }
 }
 
-public void profileData(String username){
-    String sql = "SELECT Username, FirstName, LastName, DOB FROM user WHERE Username = ?";
-    PreparedStatement stmt = coonectiion.preparedStatement(sql);
-    stmt.setString(1, username);
-    return stmt;
+public void insertRecord(String Weight, String Temp, String HighBP, String LowBP, String Date, String Username, String url){
+    String sql = "INSERT INTO record(Weight, Temp, HighBP, LowBP, Date, Username) VALUES(?,?,?,?,?,?)";
+    try(Connection conn = DriverManager.getConnection(url);)
+            {PreparedStatement stmt = conn.preparedStatement(sql);
+    stmt.setString(1, Weight);
+    stmt.setString(2, Temp);
+    stmt.setString(3, HighBP);
+    stmt.setString(4, LowBP);
+    stmt.setString(5, Date);
+    stmt.setString(6, Username);
+                stmt.execute();
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+
+    
 }
 
-public void updateProfile(String username, String firstName, String lastName, String DOB){
+public ResultSet profileData(String username, String url){
+    String sql = "SELECT Username, FirstName, LastName, DOB FROM user WHERE Username = ?";
+    try(Connection conn = DriverManager.getConnection(url);)
+            {PreparedStatement stmt = conn.preparedStatement(sql);
+                stmt.setString(1, username);
+            return stmt.executeQueary();
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+}
+
+public void updateProfile(String username, String firstName, String lastName, String DOB, String url){
     String sql = "UPDATE user SET Username = ?, FirstName = ?, LastName = ?, DOB = ? WHERE Username = ?";
+    try(Connection conn = DriverManager.getConnection(url);){
     PreparedStatement stmt = coonectiion.preparedStatement(sql);
     stmt.setString(1, username);
     stmt.setstring(2, firstName);
     stmt.setString(3, lastName);
     stmt.setString(4, DOB);
     stmt.setString(5, username);
-    return stmt;
+    stmt.execute();
+    }catch (SQLException e){
+                System.out.println(e.getMessage());
+    }
+
+
 }
 
-public void tableViewData(){
+
+// It makes it easier to encapsulate the data, manage it, and manipulate it, 
+// and it also makes it easier to add additional functionality to the application, 
+// such as sorting or filtering the data.
+public ResultSet tableViewData(String username, String url){
     String sql = "SELECT weight, Temp, HighBP, LowBP, Note, Date FROM record WHERE Username = ?";
-    PreparedStatement stmt = connection.preparedStatement(sql);
-    stmt.setString(1, user.getUsername());
-    return stmt;
+      try(Connection conn = DriverManager.getConnection(url);){
+    PreparedStatement stmt = coonectiion.preparedStatement(sql);
+    stmt.setString(1, username);
+    return stmt.execute();
+    }catch (SQLException e){
+                System.out.println(e.getMessage());
+    }
 }
 
-public void updateRecord(String newWeight, String newTemp, String newHighBP, String newLowBP, String recordID){
+public void updateRecord(String newWeight, String newTemp, String newHighBP, String newLowBP, String recordID, String url){
     String sql = "UPDATE record SET Weight = ?, Temp = ?, HighBP = ?, LowBP=? WHERE RecordID = ?"
+     try(Connection conn = DriverManager.getConnection(url);){
     PreparedStatement stmt = coonectiion.preparedStatement(sql);
     stmt.setString(1, newWeight);
     stmt.setstring(2, newTemp);
     stmt.setString(3, newHighBP);
     stmt.setString(4, newLowBP);
     stmt.setString(5, recordID);
-    return stmt;
-
+    stmt.execute();
+    }catch (SQLException e){
+                System.out.println(e.getMessage());
+    }
 }
 
-public void deleteRecord(String recordID){
+public void deleteRecord(String recordID String url){
     String sql = "DELETE FROM record WHERE RecordID = ?";
+     try(Connection conn = DriverManager.getConnection(url);){
     PreparedStatement stmt = coonectiion.preparedStatement(sql);
     stmt.setString(1, recordID);
-    return stmt;
+    stmt.execute();
+    }catch (SQLException e){
+                System.out.println(e.getMessage());
+    }
 }
